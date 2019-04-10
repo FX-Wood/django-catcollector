7c20c2a953e776acc390fc81d66c68197917e635
FX-Wood <fxwood2@gmail.com>
2019-04-10 10:32:46 -0700

    Adding crud operations and views for cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Create your views here.
@@ -16,3 +17,23 @@ def cats_index(request):
 def cats_show(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/show.html', {'cat': cat})
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        # we are gonna add some stuff here
+        self.object.save()
+        return HttpResponseRedirect(f"/cats/{str(self.object.pk)}")
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
